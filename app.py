fc0e481489734b92c82a59315e4a45f8292d27cd
spaceshuttl <nikolashenrijs@gmail.com>
2025-11-02 19:08:07 +0200

    Turpinājums pmatnes izstrādei

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, redirect, url_for, render_template, request
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-
+bcrypt = Bcrypt(app)
 # visas lapas ir definētas, kā funkcijas, kas ved uz kādu html failu no templates mapes, un vēl padod tam datus
 
-@app.route('/')
+@app.route('/home')
 def home():  # put application's code here
     return render_template('home.html')
 
 @app.route('/orders')
 def order():  # put application's code here
-    return render_template('orders.html', order_amount=7, order_list=['00001', '00002', '00003', '00004', '00005', '00006', '00007'])
+    return render_template('orders/orders.html', order_amount=7, order_list=['00001', '00002', '00003', '00004', '00005', '00006', '00007'])
 
 @app.route('/orders/<orderid>')
 def order_by_id(orderid):  # put application's code here
-    return render_template('order_details.html', order_id=orderid)
+    return render_template('orders/order_details.html', order_id=orderid)
 
 @app.route('/<name>')
 def driver(name):  # put application's code here
@@ -25,13 +26,18 @@ def admin():  # put application's code here
     # return redirect(url_for('hello_world'))
     return redirect(url_for('driver', name='admin_1'))
 
-@app.route('/login', methods=['GET', 'POST']) #login url, kas var ari darit post ne tikai get (parejiem defaultaa tikai get)
+@app.route('/', methods=['GET', 'POST']) #login url, kas var ari darit post ne tikai get (parejiem defaultaa tikai get)
 def login():
     if request.method == 'POST':
         username = request.form['email']
         password = request.form['pwd']
-        if username != 'admin' or password != '123':
+        f = open('passwords.txt')
+        stored_hash = f.readline()
+        f.close()
+        if username != 'admin' or bcrypt.check_password_hash(stored_hash, password) != True: #PAROLE IR "qwerty"
+            del password, stored_hash
             return redirect(url_for('login'))
+        del password, stored_hash
         return render_template('profile.html', user=username)
     else:
         return render_template('login.html')
