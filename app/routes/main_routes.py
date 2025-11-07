58aa48d81f5f076994b1bf1f74555481a25485e7
spaceshuttl <nikolashenrijs@gmail.com>
2025-11-08 01:27:14 +0200

    Dazi labojumi, lai radas home page, kad atver ip adresi

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/home')
+@main_bp.route('/')
 def home():
     return render_template('home.html')
 
