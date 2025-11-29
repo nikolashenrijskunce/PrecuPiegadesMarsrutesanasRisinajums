dcdc9c126e3e1702684ad9dddf7035c83c6e5042
spaceshuttl <nikolashenrijs@gmail.com>
2025-11-29 20:19:45 +0200

    Talaka izstrade ar algoritmu prieks mrsrutesanas

--- app/utils/route_calc.py
+++ app/utils/route_calc.py
@@ -3,13 +3,14 @@
 # def calculate():
 #     return
 
-import requests
 import json
 import urllib
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
 
 
+# Ievada API key un informāciju par pasūtījuma adresēm
 def create_data():
-  """Creates the data."""
   data = {}
   data['API_key'] = 'YOUR_API_KEY'
   data['addresses'] = ['3610+Hacks+Cross+Rd+Memphis+TN', # depot
@@ -31,6 +32,7 @@ def create_data():
                       ]
   return data
 
+# Nosaka secīgās darbības informācijas padošanai un distanču matricas izveidei un formatēšanai
 def create_distance_matrix(data):
   addresses = data["addresses"]
   API_key = data["API_key"]
@@ -56,8 +58,9 @@ def create_distance_matrix(data):
     distance_matrix += build_distance_matrix(response)
   return distance_matrix
 
+# Veic distan;cu matricas pieprasījumu Google serveriem
 def send_request(origin_addresses, dest_addresses, API_key):
-  """ Build and send request for the given origin and destination addresses."""
+  # Noformatē pieprasījumu, kas tiks izsūtīts Google serveriem
   def build_address_str(addresses):
     # Build a pipe-separated string of addresses
     address_str = ''
@@ -75,6 +78,7 @@ def send_request(origin_addresses, dest_addresses, API_key):
   response = json.loads(jsonResult)
   return response
 
+# Pārveido saņemtās diztanču matricas informāciju saraksta fomrātā
 def build_distance_matrix(response):
   distance_matrix = []
   for row in response['rows']:
@@ -82,9 +86,7 @@ def build_distance_matrix(response):
     distance_matrix.append(row_list)
   return distance_matrix
 
-########
-# Main #
-########
+# Veic visa aprēķinu faila darbību
 def calculation():
   """Entry point of the program"""
   # Create the data.
@@ -93,3 +95,93 @@ def calculation():
   API_key = data['API_key']
   distance_matrix = create_distance_matrix(data)
   print(distance_matrix)
+  return distance_matrix
+
+###########################################################################################################
+# Vienkarsi izvada risinajumu konsole
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()}")
+    max_route_distance = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        if not routing.IsVehicleUsed(solution, vehicle_id):
+            continue
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += f" {manager.IndexToNode(index)} -> "
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id
+            )
+        plan_output += f"{manager.IndexToNode(index)}\n"
+        plan_output += f"Distance of the route: {route_distance}m\n"
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print(f"Maximum of the route distances: {max_route_distance}m")
+###########################################################################################################
+
+# Apkopo ievades datus algoritmam ar vārdnīcu
+def create_data_model():
+    """Stores the data for the problem."""
+    data = {}
+    data["distance_matrix"] = calculation()
+    data["num_vehicles"] = 4
+    data["depot"] = 0
+    return data
+
+# Izveido optimālos maršrutus piegādēm uz vairākām vietām
+def main():
+    """Entry point of the program."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
+    )
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["distance_matrix"][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    dimension_name = "Distance"
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        3000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name,
+    )
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+    else:
+        print("No solution found !")
